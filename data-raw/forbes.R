4bbab4ffa6601378a2662ed446e0d0944df55d67
Ben Baumer <ben.baumer@gmail.com>
2018-11-19 14:25:01 -0500

    updated Forbes valuations

--- data-raw/forbes.R
+++ data-raw/forbes.R
@@ -18,3 +18,25 @@ d <- read.dta(here("data-raw", "2011-07-29.dta"))
 franchises <- d %>%
   select(t_id, club, team, year = yr, revenue, metropop, percapinc, est500)
 
+
+# Older stuff
+
+rev_old <- readxl::read_excel(here("data-raw/MLBhistorical.xls"),
+                        sheet = "MLB - Revenue", range = "B4:O34") %>%
+  gather(key = "Year", value = "Revenue", -Team) %>%
+  mutate(Year = parse_number(Year),
+         Team = gsub("Anaheim Angels", "Los Angeles Angels", Team),
+         Team = gsub("Florida Marlins", "Miami Marlins", Team),
+         Team = gsub("Montreal Expos", "Washington Nationals", Team))
+
+value_old <- readxl::read_excel(here("data-raw/MLBhistorical.xls"),
+                              sheet = "MLB - Valuation", range = "B4:O34") %>%
+  gather(key = "Year", value = "Valuation", -Team) %>%
+  mutate(Year = parse_number(Year),
+         Team = gsub(" of Anaheim", "", Team),
+         Team = gsub("Florida Marlins", "Miami Marlins", Team))
+
+forbes_old <- rev_old %>%
+  inner_join(value_old, by = c("Team", "Year"))
+
+
